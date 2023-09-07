b3b82754883532ffb2af2f6003e15a85ea802484
yesiddev <yesiddeveloper@gmail.com>
2023-09-07 13:38:41 -0500

    add new plugin

---
+++ lua/yesidev/plugins/hop.lua
@@ -0,0 +1,27 @@
+return {
+	"phaazon/hop.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		-- import hop plugin
+		local hop = require("hop")
+
+		-- enable hop
+		hop.setup()
+
+		-- keybind options
+		local opts = { noremap = true, silent = true }
+
+		-- set keybinds
+		opts.desc = "HopChar before cursor"
+		vim.keymap.set("n", "su", "<cmd>HopChar2BC<CR>", opts)
+
+		opts.desc = "HopChar after cursor"
+		vim.keymap.set("n", "se", "<cmd>HopChar2AC<CR>", opts)
+
+		opts.desc = "HopLine"
+		vim.keymap.set("n", "sn", "<cmd>HopLine<CR>", opts)
+
+		opts.desc = "HopCurrentLine"
+		vim.keymap.set("n", "sa", "<cmd>HopChar1CurrentLine<CR>", opts)
+	end,
+}

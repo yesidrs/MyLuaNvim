6789c1aa81adc0b001b14c2bcbbfa914972ae866
yesiddev <yesiddeveloper@gmail.com>
2023-10-09 13:28:37 -0500

    add my initial configs to treesitter text-objects

--- lua/yesidev/plugins/nvim-treesitter.lua
+++ lua/yesidev/plugins/nvim-treesitter.lua
@@ -14,6 +14,71 @@ return {
 
 			-- configure treesitter
 			treesitter.setup({ -- enable syntax highlighting
+
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["ii"] = "@conditional.inner",
+							["ai"] = "@conditional.outer",
+							["il"] = "@loop.inner",
+							["al"] = "@loop.outer",
+							["at"] = "@comment.outer",
+						},
+					},
+
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							[";f"] = "@function.outer",
+							[";c"] = "@class.outer",
+							[";i"] = "@conditional.outer",
+							[";l"] = "@loop.*",
+							[";t"] = "@comment.outer",
+						},
+
+						goto_previous_start = {
+							[";ff"] = "@function.outer",
+							[";cc"] = "@class.outer",
+							[";ii"] = "@conditional.outer",
+							[";ll"] = "@loop.*",
+							[";tt"] = "@comment.outer",
+						},
+
+						goto_next_end = {
+							["-f"] = "@function.outer",
+							["-c"] = "@class.outer",
+							["-i"] = "@conditional.outer",
+							["-l"] = "@loop.outer",
+							["-t"] = "@comment.outer",
+						},
+
+						goto_previous_end = {
+							["--"] = "@function.outer",
+							["-;"] = "@class.outer",
+						},
+					},
+
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
+					},
+				},
+
 				highlight = {
 					enable = true,
 				},
@@ -40,28 +105,7 @@ return {
 					enable_autocmd = false,
 				},
 				-- auto install above language parsers
-				auto_install = true,
-
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-							["ii"] = "@conditional.inner",
-							["ai"] = "@conditional.outer",
-							["il"] = "@loop.inner",
-							["al"] = "@loop.outer",
-							["at"] = "@comment.outer",
-						},
-					},
-				},
+				auto_install = true, -- enable nvim-ts-rainbow plugin
 
 				rainbow = {
 					enable = true,

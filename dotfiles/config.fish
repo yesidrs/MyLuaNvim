98d41fac6aa6413328bce95da46d1b9a5616ad5f
yesiddev <yesiddeveloper@gmail.com>
2023-10-19 19:23:02 -0500

    add fish configs

---
+++ dotfiles/config.fish
@@ -0,0 +1,23 @@
+set fish_greeting
+# set -g theme_color_scheme gruvbox
+
+alias cl "clear"
+alias tf "terraform"
+alias ll "lsd -l"
+alias la "lsd -a"
+alias ls "lsd"
+
+fzf_configure_bindings --directory=\cf
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+set -gx PATH $PATH /home/yesidev/.local/bin
+set -gx PATH $PATH ~/learning/bash/
+
+# set -x EDITOR "neovim"
+# set -x GPU "GeForce RTX 3050"
+# set -x CPU "AMD Ryzen 7 5700X 8-Core Processor"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH

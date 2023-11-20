7dd578086656305753b7abd2a00035c51c0806bd
yesidev <yesiddeveloper@gmail.com>
2023-11-19 19:18:48 -0500

    add new plugins

---
+++ lua/yesidev/plugins/gen.lua
@@ -0,0 +1,12 @@
+return {
+	"David-Kunz/gen.nvim",
+	config = function()
+		require("gen").prompts["DevOps me!"] = {
+			prompt = "You Are a senior devops engineer, acting as an assistant. You offer help with cloud technologies like: Terraform, AWS, Kubernetes, Python, Azure DevOps yaml pipelines. You answer with code examples when possible. $input:\n$text",
+			replace = true,
+		}
+
+		vim.keymap.set("v", "<leader>]", ":Gen<CR>")
+		vim.keymap.set("n", "<leader>]", ":Gen<CR>")
+	end,
+}

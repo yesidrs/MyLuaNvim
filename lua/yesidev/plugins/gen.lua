21a139f8dfc412704f415a1595883c4e4f31c9b6
yesiddev <yesiddeveloper@gmail.com>
2024-01-24 22:55:29 -0500

    change ia model

--- lua/yesidev/plugins/gen.lua
+++ lua/yesidev/plugins/gen.lua
@@ -1,6 +1,9 @@
 return {
 	"David-Kunz/gen.nvim",
 	config = function()
+		require("gen").model = "codellama"
+		require("gen").container = "ollama"
+
 		require("gen").prompts["DevOps me!"] = {
 			prompt = "You Are a senior devops engineer, acting as an assistant. You offer help with cloud technologies like: Terraform, AWS, Kubernetes, Python, Azure DevOps yaml pipelines. You answer with code examples when possible. $input:\n$text",
 			replace = true,
@@ -11,7 +14,7 @@ return {
 			replace = false,
 		}
 
-		vim.keymap.set("v", "<leader>;", ":Gen<CR>")
-		vim.keymap.set("n", "<leader>;", ":Gen<CR>")
+		vim.keymap.set("v", "<leader>/", ":Gen<CR>")
+		vim.keymap.set("n", "<leader>/", ":Gen<CR>")
 	end,
 }
